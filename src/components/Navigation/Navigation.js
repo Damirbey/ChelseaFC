097a43592b1419ee492ec2e72b718c5e9d9353c4
Damirbey <dydamirbey@gmail.com>
2021-07-30 00:12:28 -0400

    Fixed navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,15 +3,37 @@ import './Navigation.css';
 function Navigation()
 {
     return (
-        <nav>
-            <ul className="main-nav">
-                <li><a href="#"><i class="fa fa-home fa-2x"></i></a></li>
-                <li><a href="#">Chelsea FC</a></li>
-                <li><a href="#">Squad</a></li>
-                <li><a href="#">Transfers</a></li>
-                <li><a href="#">Statistics</a></li>
-                <li className="push"><a href="#">Login new</a></li>
-            </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <a class="navbar-brand" href="#">
+                <i class="fa fa-home fa-2x"></i>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Chelsea FC</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Squad</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Transfers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Statistics</a>
+                    </li>              
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-user-plus"></i> Sign Up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-sign-in"></i> Login</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
