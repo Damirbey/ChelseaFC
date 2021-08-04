df642ce776287a1357c453d8708f4dd027d2b4a6
Damirbey <dydamirbey@gmail.com>
2021-08-04 00:11:42 -0400

    Completed Squad page

--- src/components/Squad/Squad.js
+++ src/components/Squad/Squad.js
@@ -5,27 +5,167 @@ function Squad(){
     return(
         <div>
             <h3>Goalkeepers</h3>
-            
+
             <div className="squadInfo">
             
-                <div className="playerInfo">
+                <atricle className="playerInfo">
                     
                     <img src={process.env.PUBLIC_URL+"/images/Squad/cabalero.png"}/>
                     <div>
-                        <p><i class="fa fa-user"></i> Eduard Mendy</p>
-                        <p><i class="fa fa-trophy"></i> Games Played: 25</p>
+                        <h5><i class="fa fa-user"></i> Eduard Mendy</h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 25</p>
                     </div>
-                </div>
+                </atricle>
 
-                <div className="playerInfo">
+                <article className="playerInfo">
                     
                     <img src={process.env.PUBLIC_URL+"/images/Squad/kepa.png"}/>
                     <div>
-                        <p><i class="fa fa-user"></i> Kepa Arizabalaga </p>
-                        <p><i class="fa fa-trophy"></i> Games Played: 17 </p>
+                        <h5><i class="fa fa-user"></i> Kepa Arizabalaga </h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 17 </p>
                     </div>
-                </div>
+                </article>
                 
+            </div>
+
+            <h3>Defenders</h3>
+
+            <div className="squadInfo">
+            
+            <atricle className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/cabalero.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Eduard Mendy</h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 25</p>
+                    </div>
+                </atricle>
+
+                <article className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/kepa.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Kepa Arizabalaga </h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 17 </p>
+                    </div>
+                </article>
+
+                <atricle className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/cabalero.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Eduard Mendy</h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 25</p>
+                    </div>
+                </atricle>
+
+                <article className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/kepa.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Kepa Arizabalaga </h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 17 </p>
+                    </div>
+                </article>
+
+                <atricle className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/cabalero.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Eduard Mendy</h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 25</p>
+                    </div>
+                </atricle>
+
+                <article className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/kepa.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Kepa Arizabalaga </h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 17 </p>
+                    </div>
+                </article>
+            </div>
+
+            <h3>Midfielders</h3>
+
+            <div className="squadInfo">
+            
+                <atricle className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/cabalero.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Eduard Mendy</h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 25</p>
+                    </div>
+                </atricle>
+
+                <article className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/kepa.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Kepa Arizabalaga </h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 17 </p>
+                    </div>
+                </article>
+
+                <atricle className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/cabalero.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Eduard Mendy</h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 25</p>
+                    </div>
+                </atricle>
+
+                <article className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/kepa.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Kepa Arizabalaga </h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 17 </p>
+                    </div>
+                </article>
+
+            </div>
+
+            <h3>Forwards</h3>
+
+            <div className="squadInfo">
+                <atricle className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/cabalero.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Eduard Mendy</h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 25</p>
+                    </div>
+                </atricle>
+
+                <article className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/kepa.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Kepa Arizabalaga </h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 17 </p>
+                    </div>
+                </article>
+                <atricle className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/cabalero.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Eduard Mendy</h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 25</p>
+                    </div>
+                </atricle>
+
+                <article className="playerInfo">
+                    
+                    <img src={process.env.PUBLIC_URL+"/images/Squad/kepa.png"}/>
+                    <div>
+                        <h5><i class="fa fa-user"></i> Kepa Arizabalaga </h5>
+                        <p className="playerStats"><i class="fa fa-trophy"></i> Games Played: 17 </p>
+                    </div>
+                </article>
             </div>
         </div>
         
